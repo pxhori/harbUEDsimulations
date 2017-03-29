035ce0addab46dcc3a95c4cda0b0c16f7237b0dc
pxhori <pxhori@villanova.edu>
2017-03-29 16:16:09 -0400

    Changes log file immediately so that you can graph the results as they come out instead of having to wait for the end

--- autocaller.py
+++ autocaller.py
@@ -14,15 +14,23 @@ with open("Parameters.txt") as f:
         parameters.append(row)
 lssrange=[float(x) for x in parameters[0] if not ''==x]
 Nrange=[int(float(x)) for x in parameters[1] if not ''==x]
+#create log file
 i=1
 f=open("log.txt", "w")
 f.write("run,lss,N\n")
 for lss in lssrange:
     for N in Nrange:
-        os.system("python EZSim.py --lss={0:.20f} --N={1:} ".format(lss, N)+"--describe='Run_"+str(i)+"'")
         f.write("{},{},{}\n".format(i,lss,N))      
-        time.sleep(.1)
-        print("\nRun "+str(i)+" is finished! \n")
         i+=1
 f.close()
 os.system("cp log.txt statistics/")
+#Actually run the program
+i=1
+for lss in lssrange:
+    for N in Nrange:
+        os.system("python EZSim.py --lss={0:.20f} --N={1:} ".format(lss, N)+"--describe='Run_"+str(i)+"'")     
+        time.sleep(.1)
+        print("\nRun "+str(i)+" is finished! \n")
+        i+=1
+
+
